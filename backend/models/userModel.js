94125a31c24e82380a2c8b7a95d1870b6d9a1e43
im-satyam <157481814+im-satyam@users.noreply.github.com>
2025-03-05 11:32:52 +0530

    Adding the authentication

---
+++ backend/models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Name is required"],
+    },
+    username: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: [true, "Password is required"],
+    },
+    batch: {
+      type: String,
+      require: [true, "Batch is required"],
+    },
+    course: {
+      type: String,
+      require: [true, "Course is required"],
+      enum: ["B.Tech", "BCA", "BBA", "B.Com"],
+    },
+    location: {
+      type: String,
+      require: [true, "location is required"],
+    },
+    phone: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+    },
+    profession: {
+      type: String,
+      require: [true, "Email is required"],
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("users", userSchema);
